56dccaaf861d3b8b89d431f431c78cd2cc0a5271
Elise <iskateng@hotmail.com>
2022-07-17 23:22:00 -0400

    1

---
+++ Election_Analysis/PyPoll.py
@@ -0,0 +1,22 @@
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# The data we need to retrieve.
+
+#1. The total number of votes cast
+
+
+#2. A complete list of candidates who received votes
+
+
+#3. The percentage of votes each candidate won
+
+
+#4. The total number of votes each cadidtate won
+
+
+#5. The winner of the election based on popular vote
