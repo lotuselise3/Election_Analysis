1562f64cdae78aa8f9f8af1296670f0a944fec94
Elise <iskateng@hotmail.com>
2022-07-19 23:42:09 -0400

    Adding the election analysis

--- Election_Analysis/Python_practice.py
+++ Election_Analysis/Python_practice.py
@@ -20,6 +20,7 @@ counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
 for county in counties_dict:
     print(county)
 
+
 for county in counties_dict.keys():
     print(county)
 
@@ -47,3 +48,42 @@ import datetime as dt
 now = dt.datetime.now()
 # Print the present time.
 print("The time right now is ", now)
+
+import csv
+import os 
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w") 
+
+##In election_analysis.txt add "Hello World" to the first line 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+     txt_file.write("Counties in the Election\n \nArapahoe\nDenver\nJefferson")
+
+
+#1. The total number of votes cast
+
+
+#2. A complete list of candidates who received votes
+
+
+#3. The percentage of votes each candidate won
+
+
+#4. The total number of votes each cadidtate won
+
+
+#5. The winner of the election based on popular vote
